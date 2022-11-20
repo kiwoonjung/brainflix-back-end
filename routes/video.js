05e000b34950d1b3d8f58fb976840e61580ae813
kiwoonjung <kiwoon0627@gmail.com>
2022-11-19 20:12:30 -0800

    add Implement POST

--- routes/video.js
+++ routes/video.js
@@ -44,4 +44,47 @@ router.route("/:id").get((req, res) => {
   res.json(foundVideo);
 });
 
+router
+  .route("/:id/comments")
+  .get((req, res) => {
+    const data = fs.readFileSync("./data/video-details.json", "utf-8");
+    res.json(JSON.parse(data));
+  })
+  .post((req, res) => {
+    const data = fs.readFileSync("./data/video-details.json", "utf-8");
+    const videoData = JSON.parse(data);
+
+    console.log(videoData);
+    if (req.body && req.body.comment) {
+      const updateComment = {
+        id: uuid(),
+        name: "Kiwoon",
+        comment: req.body.comment,
+        likes: "0",
+        timestamp: Date.now(),
+      };
+
+      //for loop method
+      // for (let i = 0; i < videoData.length; i++) {
+      //   if (videoData[i].id === req.params.id) {
+      //     videoData[i].comments.push(updateComment);
+      //   }
+      // }
+
+      const newVideoData = videoData.map((video) => {
+        if (video.id === req.params.id) {
+          video.comments.push(updateComment);
+        }
+        return video;
+      });
+      fs.writeFileSync(
+        "./data/video-details.json",
+        JSON.stringify(newVideoData)
+      );
+      res.send("comment updated");
+    } else {
+      res.send("You forgot to include json data in your request");
+    }
+  });
+
 module.exports = router;
